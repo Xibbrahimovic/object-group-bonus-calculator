16c4555b0c5b2ed978d3752eb5cfef2626e5d14f
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-09-28 15:00:44 -0500

    first conditional complete

--- client.js
+++ client.js
@@ -40,3 +40,67 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+//creating final object (output of function)
+let finalPay = {
+  name: "",
+  bonusPercentage: 0,
+  totalCompensation: 0,
+  totalBonus: 0 
+}
+
+function reviewRatingBonus(name){
+  let bonus = 0;
+  if (employee.reviewRating <= 2) {
+    bonus += 0;
+  }//end if for 2star 
+  else if (employee.reviewRating === 3) {
+    bonus += (employee.annualSalary * .04);
+  }//end else if for 3star
+  else if (employee.reviewRating === 4) {
+    bonus += (employee.annualSalary * .06);
+  }//end else if for 4star
+  else(employee.reviewRating === 5) {
+    bonus += (employee.annualSalary * .1)
+  }//end else if for 5star
+  return bonus;
+}
+
+//function that takes in employee name and has output of object
+function getBonus(name) {
+  //create variables
+  const maxBonus = .13;
+  const minBonus = 0;
+//loop through employees array to find name match
+for (let i = 0; i < employees.length; i++) {
+  // if (name === employee[i].name) {
+    let bonus = 0;//running total
+
+    if (employees[i].reviewRating <= 2) {//if conditional for reviewRating
+      bonus += 0;
+    }//end if for 2star 
+    else if (employees[i].reviewRating === 3) {
+      bonus += .04;
+    }//end else if for 3star
+    else if (employees[i].reviewRating === 4) {
+      bonus += .06;
+    }//end else if for 4star
+    else(employees[i].reviewRating === 5) {
+      bonus +=  .1;
+    }//end else if for 5star
+    //if conditional for digits
+    if(employees[i].employeeNumber.length <= 4){
+      bonus += .15;
+    }
+
+    //if conditional for annual income
+    if(employees[i].annualSalary > 65000){
+      bonus -= .01;``
+    }
+      
+  
+  // }//end if accessing name
+  
+}//end for
+}//end getBonus
+
