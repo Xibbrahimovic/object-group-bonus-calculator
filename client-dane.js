2bb4fd6673541be0a1db12c87cbd59f2ab3d15a3
Kongxib Vue <kongcvue@gmail.com>
2021-09-29 10:12:11 -0500

    just trying danes code

--- client-dane.js
+++ client-dane.js
@@ -1,29 +1,89 @@
+const employees = [
+  {
+    name: "Atticus",
+    employeeNumber: "2405",
+    annualSalary: "47000",
+    reviewRating: 3,
+  },
+  {
+    name: "Jem",
+    employeeNumber: "62347",
+    annualSalary: "63500",
+    reviewRating: 4,
+  },
+  {
+    name: "Scout",
+    employeeNumber: "6243",
+    annualSalary: "74750",
+    reviewRating: 5,
+  },
+  {
+    name: "Robert",
+    employeeNumber: "26835",
+    annualSalary: "66000",
+    reviewRating: 1,
+  },
+  {
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "35000",
+    reviewRating: 1,
+  },
+];
+
 console.log(employees);
 
-
-
 function calculate(employee) {
-    console.log(employee);
-
-    // if (employee.reviewRating === 3) {
-    //     console.log('its 3');
-    // }
-switch (employee.reviewRating) {
+  console.log(employee);
+  let baseBonus = 0;
+  // if (employee.reviewRating === 3) {
+  //     console.log('its 3');
+  // }
+  switch (employee.reviewRating) {
     case 1:
     case 2:
-        console.log('No bonus!');
-        break;
+      console.log("No bonus!");
+      baseBonus = 0;
+      break;
     case 3:
-        console.log('it is 3!');
-        break;
-    case 4: 
-        console.log('its 4');
+      console.log("it is 3!");
+      baseBonus = 4;
+      break;
+    case 4:
+      console.log("its 4");
+      baseBonus = 6;
+      break;
     case 5:
-        console.log('its 5');
+      console.log("its 5");
+      baseBonus = 10;
+      break;
     default:
-        alert('ERROR check the bonus rating')
+      alert("ERROR check the bonus rating");
+  }
+
+  //if early employee, add 5
+  if (employee.employeeNumber.length <= 4) {
+    baseBonus += 5;
+  }
+  //if salary is higher than 65,000, adjust down by 1
+  if (employee.annualSalary > 65000) {
+    baseBonus -= 1;
+  }
+
+  if (baseBonus > maxBonus) {
+    baseBonus = maxBonus;
+  } else if (baseBonus < minBonus) {
+    baseBonus = minBonus;
+  }
+
+  return {
+    name: employee.name,
+    bonusPercentage: `$(baseBonus)%`,
+    totalCompensation: Number(employee.annualSalary) + totalBonus,
+    totalBonus: totalBonus,
+  };
 }
-}
+let result = calculateBonus(employee[2]);
+console.log(result);
 
 calculate(employee[0]);
-
--- client.js
+++ client.js
@@ -1,34 +1,34 @@
 const employees = [
   {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
+    name: "Atticus",
+    employeeNumber: "2405",
+    annualSalary: "47000",
+    reviewRating: 3,
   },
   {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
+    name: "Jem",
+    employeeNumber: "62347",
+    annualSalary: "63500",
+    reviewRating: 4,
   },
   {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
+    name: "Scout",
+    employeeNumber: "6243",
+    annualSalary: "74750",
+    reviewRating: 5,
   },
   {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
+    name: "Robert",
+    employeeNumber: "26835",
+    annualSalary: "66000",
+    reviewRating: 1,
   },
   {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "35000",
+    reviewRating: 1,
+  },
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -43,33 +43,33 @@ const employees = [
 
 //creating final object (output of function)
 
-
 function getOneBonus(employee) {
   let bonus = 0;
-  const maxBonus = .13;
+  const maxBonus = 0.13;
   const minBonus = 0;
   for (let i = 0; i < employees.length; i++) {
     if (employee === employees[i].name) {
-      if (employees[i].reviewRating <= 2) { //if conditional for reviewRating
+      if (employees[i].reviewRating <= 2) {
+        //if conditional for reviewRating
         bonus += 0;
-      } //end if for 2star 
+      } //end if for 2star
       else if (employees[i].reviewRating === 3) {
-        bonus += .04;
+        bonus += 0.04;
       } //end else if for 3star
       else if (employees[i].reviewRating === 4) {
-        bonus += .06;
+        bonus += 0.06;
       } //end else if for 4star
       else if (employees[i].reviewRating === 5) {
-        bonus += .1;
+        bonus += 0.1;
       } //end else if for 5star
       //if conditional for digits
       if (employees[i].employeeNumber.length <= 4) {
-        bonus += .05;
+        bonus += 0.05;
       }
 
       //if conditional for annual income
       if (employees[i].annualSalary > 65000) {
-        bonus -= .01;
+        bonus -= 0.01;
       }
 
       if (bonus > maxBonus) {
@@ -77,29 +77,28 @@ function getOneBonus(employee) {
       } else if (bonus < minBonus) {
         bonus = minBonus;
       }
-      
+
       let finalPayForEmployee = {
         name: employee,
         bonusPercentage: bonus,
-        totalCompensation: (employees[i].annualSalary * bonus) + (employees[i].annualSalary*1),
-        totalBonus: Math.round(employees[i].annualSalary * bonus)
-    };
-    
-    return finalPayForEmployee;
+        totalCompensation:
+          employees[i].annualSalary * bonus + employees[i].annualSalary * 1,
+        totalBonus: Math.round(employees[i].annualSalary * bonus),
+      };
+
+      return finalPayForEmployee;
     }
-    
   } //end for loop
-  
 } //end function
 
 //function that takes in employee name and has output of object
 function getAllBonus() {
-//   //create variables
-// //loop through employees array to find name match
-for (let i = 0; i < employees.length; i++) {
-  console.log(getOneBonus(employees[i].name));
+  //   //create variables
+  // //loop through employees array to find name match
+  for (let i = 0; i < employees.length; i++) {
+    console.log(getOneBonus(employees[i].name));
+  }
 }
-}
-console.log(getOneBonus('Atticus'));
+console.log(getOneBonus("Atticus"));
 
 getAllBonus();
